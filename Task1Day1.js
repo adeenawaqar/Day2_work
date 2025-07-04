11294fe5499de75eee69921010827e03c661353a
AdeenaWaqar <adeenawaqar05@gmail.com>
2025-07-04 12:31:21 +0500

    Add files via upload

---
+++ Task1Day1.js
@@ -0,0 +1,7 @@
+let country = "Chaina";
+let continent = "Asia";
+let population = 1.41; // in millon
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ Task2Day1.js
@@ -0,0 +1,7 @@
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);    // boolean
+console.log(typeof population);  // number
+console.log(typeof country);     // string
+console.log(typeof language);    // undefined
